86664eca3fb0981e24de873d192a215308b06035
YourLocalDuck <prince.reuben@gmail.com>
2024-08-02 12:00:59 -0400

    User API

--- backend/src/main/java/com/revature/controllers/UserController.java
+++ backend/src/main/java/com/revature/controllers/UserController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.revature.models.User;
 import com.revature.services.UserService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,6 +31,30 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(User user) {
+        User newUser = userService.addUser(user);
+        return ResponseEntity.status(201).body(newUser);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(User user) {
+        User updatedUser = userService.updateUser(user);
+        return ResponseEntity.status(201).body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
     
 }
